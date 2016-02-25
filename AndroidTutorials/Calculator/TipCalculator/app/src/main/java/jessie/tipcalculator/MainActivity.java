4d36fd5a4782b7b8984bb2976925c4b8212fb4bc
jessalbarian <albarian.jess@gmail.com>
2016-02-24 17:22:14 -0700

    Added Android tip calculator tutorial

--- AndroidTutorials/Calculator/TipCalculator/app/src/main/java/jessie/tipcalculator/MainActivity.java
+++ AndroidTutorials/Calculator/TipCalculator/app/src/main/java/jessie/tipcalculator/MainActivity.java
@@ -2,23 +2,25 @@ package jessie.tipcalculator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void calculateTip(View view){
+    public void calculateTip(){
         EditText billAmount = (EditText) findViewById(R.id.bill);
         EditText percentTip = (EditText) findViewById(R.id.percent);
 
         String text1 = billAmount.getText().toString();
         int bill = Integer.parseInt(text1);
+
         String text2 = percentTip.getText().toString();
         int tip = Integer.parseInt(text2);
 
-        int calc = bill*tip;
+        int calc = bill*(tip/100);
+        System.out.println(calc);
 
+        String.valueOf(calc);
         TextView calculatedTip = (TextView) findViewById(R.id.tip);
         calculatedTip.setText(calc);
     }
